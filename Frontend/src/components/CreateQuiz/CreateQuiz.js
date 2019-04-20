f299af7c77a0b167e40b6e7ec104b0cf0db83ead
Ye Mei <yemeirmei@gmail.com>
2019-04-20 13:37:22 -0700

    first frontend code

---
+++ Frontend/src/components/CreateQuiz/CreateQuiz.js
@@ -0,0 +1,16 @@
+import React, {Component} from 'react';
+
+class CreateQuizz extends Component{
+    render(){
+        return(
+            <div>
+                <br/>
+                <div class="container">
+                   
+                </div>
+            </div>
+        )
+    }
+}
+
+export default CreateQuizz;
